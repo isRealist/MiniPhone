52bb533c7a3e0e0be2a91811095741c6f4f4d658
isRealist <hamdudumiedie@gmail.com>
2025-12-08 19:14:59 +0700

    Update PhoneHudElement.cs

--- PhoneHudElement.cs
+++ PhoneHudElement.cs
@@ -46,7 +46,7 @@ namespace MiniPhone.UI
 
 
 
-            icon ??= mod.Helper.GameContent.Load<Texture2D>("Mods/MiniPhone/Assets/Phone16");
+            icon ??= mod.Helper.ModContent.Load<Texture2D>("Mods/MiniPhone/Assets/Phone16.png");
 
             if (icon == null) return;
 
@@ -107,6 +107,7 @@ namespace MiniPhone.UI
     }
 
 }
+
 
 
 
